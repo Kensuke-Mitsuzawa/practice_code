8619be16f656411a8ab761ea1e4c833a82140209
kensuke-mi <kensuke-mi@is.naist.jp>
2013-05-25 03:08:17 +0900

    move work to pine

--- bayesian_hmm/nonparametric-exercise-ja-1/Samplecorpus.py
+++ bayesian_hmm/nonparametric-exercise-ja-1/Samplecorpus.py
@@ -6,9 +6,25 @@ import sys, random
 N=10
 #タグ:確率を格納する辞書Yの定義とランダム値での初期化
 Y={}
-for i in range(0, 21):
-    random_value=random.random()
-    Y.setdefault('y_'+str(i), random_value)
+#あり得るタグをすべて格納するリスト
+tag_set=[]
+#タグ番号0から21まで設定する
+for tag_number in range(0, 21):tag_set.append(tag_number)
 
-for i in range(0, N):
-    print i
+def preprocess_Y(tag_set, Y, input_file_lines):
+    token_list=[]
+    for line in input_file_lines:
+        map(lambda x: token_list.append(x.strip('/n').split()), input_file_lines)
+
+
+    '''
+    for y_i in tag_set:
+        for y_i_minus_1 in tag_set:
+            for y_i_plus_1 in tag_set:
+            '''
+            
+def main():
+    preprocess_Y(tag_set, Y, open('wiki-sample.word', 'r').readlines())
+
+if __name__=='__main__':
+    main()
