f26692abb00c7e70faba058ea22fcf7b8b7f5efc
Kensuke Mitsuzawa <kensuke-mi@is.naist.jp>
2013-05-27 17:42:35 +0900

    fixed the interpretation mistake of Y

--- bayesian_hmm/nonparametric-exercise-ja-1/Samplecorpus.py
+++ bayesian_hmm/nonparametric-exercise-ja-1/Samplecorpus.py
@@ -5,41 +5,65 @@ import sampletag
 
 #イテレーション数は適当に
 N=10
-#y_i_-1からy_iの遷移の組み合わせを保存しておくリスト
-y_i_minus_1_to_y_i={}
-#y_iからy_i_plus_1の遷移の組み合わせを保存しておくリスト
-y_i_to_y_i_plus_1={}
-#y_iからx_iが生成される組み合わせを保存しておくリスト
-y_i_to_x_i={}
+#y_iのタグを保存しておくリスト
+tag_of_y_i=[]
+#y_i自身の頻度を保存しておく
+frequency_y_i={}
+#y_i_-1からy_iの遷移の組み合わせを保存しておく
+frequency_y_i_minus_1_to_y_i={}
+#y_iからy_i_plus_1の遷移の組み合わせを保存しておく
+frequency_y_i_to_y_i_plus_1={}
+#y_iからx_iが生成される組み合わせを保存しておく
+frequency_y_i_to_x_i={}
 #あり得るタグをすべて格納するリスト
 tag_set=[]
 #タグ番号0から21まで設定する
-for tag_number in range(0, 21):tag_set.append(tag_number)
+for tag_number in range(0, 22):tag_set.append(tag_number)
 
 def preprocess_Y(tag_set, Y, input_file_lines):
     token_list=[]
     #token_listを作成しておく
     for line in input_file_lines:
         for token in line.strip('/n').split(): token_list.append(token)
+    token_number=len(token_list)
+    #y_0からy_maxまで適当にタグを割り当てる．タグは0~21だから乱数で与える
+    for y_i in range(0, token_number): tag_of_y_i.append(random.randint(0, 21))
+    #あり得るtagのbigramをすべて辞書として定義しておく．
     #多重ループになるけども，(y_i_-1,y_i),(y_i,y_i+1),(y_i,x_i)を作り出すためにこうする
     for y_i in tag_set:
+        frequency_y_i.setdefault(str(y_i), 0)
         for y_i_minus_1 in tag_set:
-            y_i_minus_1_to_y_i.setdefault(str(y_i_minus_1)+'_'+str(y_i), random.randint(0, 500))
+            frequency_y_i_minus_1_to_y_i.setdefault(str(y_i_minus_1)+'_'+str(y_i), 0)
         for y_i_plus_1 in tag_set:
-            y_i_to_y_i_plus_1.setdefault(str(y_i)+'_'+str(y_i_plus_1), random.randint(0, 500))
+            frequency_y_i_to_y_i_plus_1.setdefault(str(y_i)+'_'+str(y_i_plus_1), 0)
         for x_i in token_list:
-            y_i_to_x_i.setdefault(str(y_i)+'_'+x_i, random.randint(0, 500))
-    Y.setdefault('y_i_minus_1_to_y_i', y_i_minus_1_to_y_i)
-    Y.setdefault('y_i_to_x_i', y_i_to_x_i)
-    Y.setdefault('y_i_to_y_i_plus_1', y_i_to_y_i_plus_1)
+            frequency_y_i_to_x_i.setdefault(str(y_i)+'_'+x_i, 0)
+    #unigram, bigramのカウントを取る
+    for y_i, tag in enumerate(tag_of_y_i):
+        frequency_y_i[str(tag)]+=1
+        forward_bigram_key=str(tag_of_y_i[(y_i)-1])+'_'+str(tag)
+        frequency_y_i_minus_1_to_y_i[forward_bigram_key]+=1
+        generate_key=str(tag)+'_'+token_list[y_i]
+        frequency_y_i_to_x_i[generate_key]+=1
+        if y_i < len(tag_of_y_i)-1:
+            backward_bigram_key=str(tag)+'_'+str(tag_of_y_i[(y_i)+1])
+            frequency_y_i_to_y_i_plus_1[backward_bigram_key]+=1
+        else:
+            frequency_y_i_to_y_i_plus_1.setdefault(y_i, 1)
+    #全部，Yの下に格納
+    Y.setdefault('sequence_y_i', tag_of_y_i)
+    Y.setdefault('frequency_y_i', frequency_y_i)
+    Y.setdefault('frequency_y_i_minus_1_to_y_i', frequency_y_i_minus_1_to_y_i)
+    Y.setdefault('frequency_y_i_to_x_i', frequency_y_i_to_x_i)
+    Y.setdefault('frequency_y_i_to_y_i_plus_1', frequency_y_i_to_y_i_plus_1)
     return token_list, Y
 
 def main():
     Y={}
     token_list, Y\
         =preprocess_Y(tag_set, Y, open('wiki-sample.word', 'r').readlines())
-    for y_i in tag_set:
-        sampletag.SampleTag(token_list, y_i, tag_set, Y)
+    for y_i, tag_of_y_i in enumerate(Y['sequence_y_i']):
+        sampletag.SampleTag(token_list, y_i, Y, tag_set)
 
 if __name__=='__main__':
     main()
--- bayesian_hmm/nonparametric-exercise-ja-1/sampletag.py
+++ bayesian_hmm/nonparametric-exercise-ja-1/sampletag.py
@@ -2,21 +2,38 @@
 # -*- coding:utf-8 -*-
 __version__='2013/5/25'
 
-def dirichlet_process(tag, y_i_minus_1_to_y_i, y_i_to_x_i, y_i_to_y_i_plus_1, p):
-   pass 
+alpha=0.1
 
+def dirichlet_process(tag, y_i, Y):
+   frequency_of_y_i_minus_1=Y['frequency_y_i'][str(Y['sequence_y_i'][(y_i)-1])]
+   frequency_of_y_i=Y['frequency_y_i'][str(Y['sequence_y_i'][y_i])]
+   
+   #TODO:残りのパラメータも準備してdiriclet processの式の完成
+   #基底確率について：P_base,Tは一様分布に．P_base_Eは単語のユニグラム確率にする
+   
 
-def SampleTag(token_list, y_i, tag_set, Y):
-    #確率を格納するリスト
-    p=[]
-    #いまのタグのカウントを−１する（グラム先生のスライドには削除って書いてあるけど，たぶん間違いだと思うんだ）
-    for y_i_minus_or_plus_1 in tag_set:
-        Y['y_i_minus_1_to_y_i'][str(y_i_minus_or_plus_1)+'_'+str(y_i)]-=1
-        Y['y_i_to_y_i_plus_1'][str(y_i)+'_'+str(y_i_minus_or_plus_1)]-=1
-    for x_i in token_list: Y['y_i_to_x_i'][str(y_i)+'_'+x_i]-=1
-    #TODO diriclet-processの導出にはy_i, y_i-1のunigram頻度が必要．これも別に乱数（整数）で設定しておけばいいんだろうか？
-    '''
-    for tag in tag_set:
-        p=dirichlet_process(tag, y_i_minus_1_to_y_i, y_i_to_x_i, y_i_to_y_i_plus_1, p)
-    y_i=SampleOne(p)
-    '''
+def SampleTag(token_list, y_i, Y, tag_set):
+   """
+   y_iはY['sequence_y_i']のリストのインデックス番号
+   token_listはcorpusのtoken区切りのリスト
+   tag_setはタグの集合
+   """
+   #確率を格納するリスト
+   p=[]
+   #--------------------------------------------------------------------------------------------------------
+   #いまのタグのカウントを−１する（グラム先生のスライドには削除って書いてあるけど，たぶん間違いだと思うんだ）
+   forward_bigram_key=str(Y['sequence_y_i'][y_i-1])+'_'+str(Y['sequence_y_i'][y_i])
+   Y['frequency_y_i_minus_1_to_y_i'][forward_bigram_key]-=1
+   generate_key=str(Y['sequence_y_i'][y_i])+'_'+str(token_list[y_i])
+   Y['frequency_y_i_to_x_i'][generate_key]-=1
+   if y_i < len(Y['sequence_y_i'])-1:
+      backward_bigram_key=str(Y['sequence_y_i'][y_i])+'_'+str(Y['sequence_y_i'][(y_i)+1])
+      Y['frequency_y_i_to_y_i_plus_1'][backward_bigram_key]-=1
+   else:
+       Y['frequency_y_i_to_y_i_plus_1'].setdefault(y_i, 1)
+    #--------------------------------------------------------------------------------------------------------
+
+   for tag in tag_set:
+      p.append(dirichlet_process(tag, y_i, Y))
+   #y_i=SampleOne(p)
+
